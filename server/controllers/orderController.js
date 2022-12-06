236080f344a61006dd6f7a7921720ca794c20aab
laveezah-noor <laveezahnoor@gmail.com>
2022-12-07 01:14:08 +0500

    Feat: Order routes

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,11 +1,11 @@
-const orderSchema = require('../models/orderModel');
+const OrderModel = require('../models/orderModel');
 const catchAsyncError = require('../middelware/catchAsyncError');
 const ErrorHandler = require('../utils/errorHandler');
 const ApiFeature = require('../utils/apifeatures');
 const ProductModel = require('../models/productModel');
 
 // Create Order
-const createOrder = catchAsyncError( async (req, res, next) => {
+exports.createOrder = catchAsyncError( async (req, res, next) => {
     const {
         orderItems, 
         shippingInfo, 
@@ -14,7 +14,7 @@ const createOrder = catchAsyncError( async (req, res, next) => {
         shippingPrice, 
         totalPrice, 
         paymentInfo} = req.body;
-    const order = await orderSchema.create({
+    const order = await OrderModel.create({
         orderItems,
         shippingInfo,
         itemsPrice,
@@ -23,11 +23,75 @@ const createOrder = catchAsyncError( async (req, res, next) => {
         totalPrice,
         paymentInfo,
         paidAt: Date.now(),
-        user: req.user.id
+        user: req.user._id
     });
     res.status(200).json({
         message: "Order created successfully",
         status: 200,
         data: order
+    })
+})
+
+exports.getOrderById = catchAsyncError( async (req, res, next) => {
+    const order = await OrderModel.findById(req.params.id)
+    .populate("user", "name email");
+
+    if(!order){
+        return next(new ErrorHandler("Order not found with this id", 404));
+    }
+
+    res.status(200).json({
+        success: true,
+        data: order
+    })
+})
+
+// Get Loggedin User Order
+exports.myOrders = catchAsyncError( async (req, res, next) =>{
+    const orders = await OrderModel.find({user: req.user._id});
+  
+    res.status(200).json({
+        success: true,
+        data: orders
+    }) 
+}) 
+
+// Get all orders
+exports.updateOrder = catchAsyncError( async (req, res, next) =>{
+    const order = await OrderModel.findById(req.params.id);
+    
+    if (order.orderStatus === "Delivered"){
+        return next(new ErrorHandler("You have already delivered this order", 400));
+    }
+    order.orderItems.forEach( async item => {
+        await updateStock(item.Product, item.quantity)
+    });
+    if(req.body.orderStatus === "Delivered"){
+        order.deliveredAt = Date.now();
+    }
+    await order.save({validateBeforeSave: false});
+    res.status(200).json({
+        message: "Get Orders Successful",
+        statusCode: 200,
+        data: [order, totalAmount]
+    }) 
+}) 
+
+async function updateStock(id, quantity){
+    const product = await ProductModel.findById(id);
+    product.stock = product.stock - quantity;
+    await product.save({validateBeforeSave: false});
+}
+
+// Delete Order
+exports.deleteOrder = catchAsyncError( async (req, res, next) =>{
+    const order = await OrderModel.findById(req.params.id);
+    if(!order){
+        return next(new ErrorHandler("Order not found with this id", 404));
+    }
+    await order.remove();
+    res.status(200).json({
+        success: true,
+        message: "Order deleted successfully"
     })
 })
--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -2,14 +2,25 @@ const express = require('express');
 const router = express.Router();
 const { 
     createOrder, 
-    getOrder, 
+    getOrderById, 
     getAllOrders, 
     updateOrder, 
-    deleteOrder } 
+    deleteOrder, 
+    myOrders} 
     = require('../controllers/orderController');
 
 const { isAuthenticatedUser, authorizeRoles } = require("../middelware/authentication");
 
-router.route("/order/new").post(createOrder);
+router.route("/order/new").post(isAuthenticatedUser, createOrder);
+
+router.route("/order/:id").get(isAuthenticatedUser, authorizeRoles("admin"), getOrderById);
+
+router.route("/orders/me").get(isAuthenticatedUser, myOrders);
+
+router.route("/admin/orders").get(isAuthenticatedUser, authorizeRoles("admin"), getAllOrders);
+
+router.route("/admin/order/:id")
+.put(isAuthenticatedUser, authorizeRoles("admin"), updateOrder)
+.delete(isAuthenticatedUser, authorizeRoles("admin"), deleteOrder);
 
 module.exports = router;
